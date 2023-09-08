4a5d7431266c453cdb5f6886f6fe6eef000d6495
Daniel Nguyen <daniel.nguyen@scaccelerator.io>
2023-09-08 22:40:32 +1000

    process env next

--- frontend/src/service/API.ts
+++ frontend/src/service/API.ts
@@ -1,7 +1,7 @@
 import { Api } from "./swagger/Api";
 
 export const backendAPI = new Api({
-  baseUrl: process.env.BACKEND_URL || "http://loalhost:8080",
+  baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080",
 });
 
-export const BASE_URL = process.env.BACKEND_URL || "http://localhost:8080";
+export const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
