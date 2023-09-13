d30dc6b49e3fbded69b0e80e7030b79b9bade1f8
Neosh Sheikh <neosh11@gmail.com>
2023-09-13 16:44:30 +1000

    remove the indents.

--- frontend/src/pages/applications/new.tsx
+++ frontend/src/pages/applications/new.tsx
@@ -72,56 +72,56 @@ export default function NewApplication() {
           <Textarea
             defaultValue={`Job Title: Oompa Loompa
 
-  Location: Willy Wonka's Chocolate Factory, Chocolateville
-  
-  Job Type: Full-time, Permanent
-  
-  Job Summary:
-  Willy Wonka's Chocolate Factory is seeking energetic, enthusiastic, and hardworking Oompa Loompas to join our team in Chocolateville. As an Oompa Loompa, you will play a crucial role in the daily operations of the factory, ensuring the production of the world's finest chocolates and candies. You will be responsible for a variety of tasks, from candy-making to assisting with factory maintenance and promoting a positive and magical atmosphere.
-  
-  Key Responsibilities:
-  
-  Candy Production: Assist in the creation and packaging of delicious chocolates, candies, and confections according to Willy Wonka's secret recipes and quality standards.
-  
-  Quality Control: Inspect candies for quality, taste, and appearance, and report any issues or deviations from the expected standards.
-  
-  Factory Maintenance: Help maintain a clean and safe working environment by cleaning workstations, equipment, and common areas regularly.
-  
-  Problem Solving: Collaborate with fellow Oompa Loompas to troubleshoot and resolve production issues, ensuring smooth operations.
-  
-  Safety Compliance: Follow all safety protocols and guidelines to prevent accidents and maintain a hazard-free workspace.
-  
-  Entertainment: Occasionally participate in musical and theatrical performances to entertain factory visitors, sharing the Oompa Loompa culture and wisdom.
-  
-  Teamwork: Foster a positive and collaborative team spirit among Oompa Loompas and work together to achieve production goals.
-  
-  Creativity: Assist in developing new candy ideas and flavors to keep the factory's offerings exciting and innovative.
-  
-  Qualifications:
-  
-  Passion for chocolate and candy-making.
-  Strong attention to detail and commitment to producing high-quality products.
-  Ability to work efficiently in a fast-paced environment.
-  Excellent teamwork and communication skills.
-  Willingness to learn and adapt to new tasks and challenges.
-  Musical and theatrical talents are a plus, but not mandatory.
-  Must be adaptable to a variety of working conditions and tasks.
-  Physical Requirements:
-  
-  Ability to stand for extended periods.
-  Lift and carry heavy objects (up to 50 pounds).
-  Bend, kneel, and reach as required to perform tasks.
-  Work in a warm and sometimes humid environment.
-  Benefits:
-  
-  Competitive salary and bonus opportunities.
-  Access to a wide variety of delectable chocolates and candies.
-  Magical and enchanting work environment.
-  Opportunities for career growth within the factory.
-  Work alongside Willy Wonka, a legendary figure in the candy industry.
-  If you have a sweet tooth, a passion for candy, and a desire to be part of a whimsical world of chocolate, apply today to become an Oompa Loompa at Willy Wonka's Chocolate Factory. We look forward to welcoming you to our team and creating delightful experiences for the world!
-  
-  Note: This job description is intended to describe the general nature and level of work to be performed and is not an exhaustive list of qualifications, duties, and responsibilities associated with the role of an Oompa Loompa at Willy Wonka's Chocolate Factory.
+Location: Willy Wonka's Chocolate Factory, Chocolateville
+
+Job Type: Full-time, Permanent
+
+Job Summary:
+Willy Wonka's Chocolate Factory is seeking energetic, enthusiastic, and hardworking Oompa Loompas to join our team in Chocolateville. As an Oompa Loompa, you will play a crucial role in the daily operations of the factory, ensuring the production of the world's finest chocolates and candies. You will be responsible for a variety of tasks, from candy-making to assisting with factory maintenance and promoting a positive and magical atmosphere.
+
+Key Responsibilities:
+
+Candy Production: Assist in the creation and packaging of delicious chocolates, candies, and confections according to Willy Wonka's secret recipes and quality standards.
+
+Quality Control: Inspect candies for quality, taste, and appearance, and report any issues or deviations from the expected standards.
+
+Factory Maintenance: Help maintain a clean and safe working environment by cleaning workstations, equipment, and common areas regularly.
+
+Problem Solving: Collaborate with fellow Oompa Loompas to troubleshoot and resolve production issues, ensuring smooth operations.
+
+Safety Compliance: Follow all safety protocols and guidelines to prevent accidents and maintain a hazard-free workspace.
+
+Entertainment: Occasionally participate in musical and theatrical performances to entertain factory visitors, sharing the Oompa Loompa culture and wisdom.
+
+Teamwork: Foster a positive and collaborative team spirit among Oompa Loompas and work together to achieve production goals.
+
+Creativity: Assist in developing new candy ideas and flavors to keep the factory's offerings exciting and innovative.
+
+Qualifications:
+
+Passion for chocolate and candy-making.
+Strong attention to detail and commitment to producing high-quality products.
+Ability to work efficiently in a fast-paced environment.
+Excellent teamwork and communication skills.
+Willingness to learn and adapt to new tasks and challenges.
+Musical and theatrical talents are a plus, but not mandatory.
+Must be adaptable to a variety of working conditions and tasks.
+Physical Requirements:
+
+Ability to stand for extended periods.
+Lift and carry heavy objects (up to 50 pounds).
+Bend, kneel, and reach as required to perform tasks.
+Work in a warm and sometimes humid environment.
+Benefits:
+
+Competitive salary and bonus opportunities.
+Access to a wide variety of delectable chocolates and candies.
+Magical and enchanting work environment.
+Opportunities for career growth within the factory.
+Work alongside Willy Wonka, a legendary figure in the candy industry.
+If you have a sweet tooth, a passion for candy, and a desire to be part of a whimsical world of chocolate, apply today to become an Oompa Loompa at Willy Wonka's Chocolate Factory. We look forward to welcoming you to our team and creating delightful experiences for the world!
+
+Note: This job description is intended to describe the general nature and level of work to be performed and is not an exhaustive list of qualifications, duties, and responsibilities associated with the role of an Oompa Loompa at Willy Wonka's Chocolate Factory.
           `}
             className="h-full box-border resize-none outline-white focus-visible:ring-0"
             placeholder="Copy and paste a job description here"
